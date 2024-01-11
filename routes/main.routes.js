c76c2aaf1d6e6d0518be40c4fb9f4449fffeccaf
Ayana888 <abbolotova2024@gmail.com>
2024-01-11 20:30:08 +0300

    routes

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,10 +1,9 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const MainPage = require("../components/MainPage");
 
-const MainPage = require('../components/MainPage');
-
-router.get('/', (req, res) => {
-  const html = res.renderComponent(MainPage, { title: 'Main page' })
-  res.send(html)
+router.get("/", (req, res) => {
+  const html = res.renderComponent(MainPage, { title: "Main page" });
+  res.send(html);
 });
 
 module.exports = router;
